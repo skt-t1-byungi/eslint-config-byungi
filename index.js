2c31d6c5e7a1743df2543bf1072f8e2513a50f10
skt-t1-byungi <tiniwz@gmail.com>
2018-03-09 09:54:12 +0900

    update react, vue, old

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 module.exports = {
   extends: [
     "standard", 
-    "plugin:react/recommended",
     "plugin:ava/recommended"
   ],
-  plugins: ["ava", "react"]
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module"
+  },
+  plugins: ["ava", "unicorn"],
+  rules:{
+    "ava/no-skip-test": "warn",
+    "unicorn/catch-error-name": ["error", {"name": "err"}],
+    "unicorn/explicit-length-check": "error",
+    "unicorn/no-array-instanceof": "error",
+    "unicorn/throw-new-error": "error",
+    "unicorn/prefer-starts-ends-with": "error",
+    "unicorn/prefer-type-error": "error",
+    "unicorn/import-index": "error",
+    "unicorn/error-message": "error"
+  }
 }
---
+++ react.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: [
+    "./index.js",
+    "plugin:react/recommended"
+  ]
+}
