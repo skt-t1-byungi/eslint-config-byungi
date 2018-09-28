148182dfa5c367c5c68f8f1780ce752a43870445
skt-t1-byungi <tiniwz@gmail.com>
2018-09-28 16:57:57 +0900

    react 4space

--- react.js
+++ react.js
@@ -1,7 +1,9 @@
 module.exports = {
     "parser": "babel-eslint",
     "rules": {
-        "strict": 0
+        "strict": 0,
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4]
     },
     extends: [
         "./index.js",
