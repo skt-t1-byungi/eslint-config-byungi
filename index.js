4df79aa5a7c43a63523595d8c89b1573d91bfdaf
skt-t1-byungi <tiniwz@gmail.com>
2019-01-02 20:54:43 +0900

    remove error name rule

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ module.exports = {
         "require-await": "error",
         "ava/no-only-test": "off",
         "ava/no-skip-test": "off",
-        "unicorn/catch-error-name": ["error", {"name": "err"}],
         "unicorn/explicit-length-check": "error",
         "unicorn/no-array-instanceof": "error",
         "unicorn/throw-new-error": "error",
