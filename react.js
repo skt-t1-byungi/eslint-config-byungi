90ff749cfe8a65362b924ae16075138782b5d130
skt-t1-byungi <tiniwz@gmail.com>
2019-09-30 22:46:49 +0900

    fix: react/jsx-handler-names to off

--- react.js
+++ react.js
@@ -9,6 +9,7 @@ module.exports = {
     rules: {
         "react/jsx-indent": ["error", 4],
         "react/jsx-indent-props": ["error", 4],
+        "react/jsx-handler-names": "off",
         "react-hooks/rules-of-hooks": "error",
         "react-hooks/exhaustive-deps": "warn"
     }
