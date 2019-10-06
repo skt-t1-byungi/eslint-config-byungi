c24ac8ddc46f930fb4808c7a79f40fd8070c8ef5
skt-t1-byungi <tiniwz@gmail.com>
2019-10-06 14:59:56 +0900

    feat: typescript

---
+++ typescript.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "extends": [
+        "./index.js",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "rules": {
+        "@typescript-eslint/array-type": ["error", {"default": "array-simple"}],
+        "camelcase": "off",
+        "@typescript-eslint/camelcase": ["error", { "properties": "never" }],
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-use-before-define": "off"
+    }
+}
