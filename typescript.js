05c427c70bd97fb18e9faafdb48fbc0cf19fa022
skt-t1-byungi <tiniwz@gmail.com>
2019-10-06 16:49:46 +0900

    fix allow empty function

--- typescript.js
+++ typescript.js
@@ -15,6 +15,7 @@ module.exports = {
         "@typescript-eslint/camelcase": ["error", { "properties": "never" }],
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-use-before-define": "off",
-        "@typescript-eslint/no-explicit-any": "off"
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-function": "off"
     }
 }
