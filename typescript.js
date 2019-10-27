444b4273b08aeede04e089f813c6ca49a03fc9fd
skt-t1-byungi <tiniwz@gmail.com>
2019-10-28 00:59:10 +0900

    fix:  member accessibility set `no-public`

--- typescript.js
+++ typescript.js
@@ -16,6 +16,7 @@ module.exports = {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-use-before-define": "off",
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-empty-function": "off"
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/explicit-member-accessibility": ["error", { "accessibility": "no-public" }]
     }
 }
