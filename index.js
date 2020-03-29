f5ae880d3dfe3f3df47d8ffe269d869751cc263c
skt-t1-byungi <tiniwz@gmail.com>
2020-03-29 23:00:05 +0900

    fix

--- index.js
+++ index.js
@@ -32,13 +32,7 @@ module.exports = {
                 ['^\\u0000']
             ]
         }],
-        'babel/new-cap': 1,
-        'babel/camelcase': 1,
-        'babel/no-invalid-this': 1,
-        'babel/object-curly-spacing': 1,
-        'babel/quotes': 1,
-        'babel/semi': 1,
-        'babel/no-unused-expressions': 1,
-        'babel/valid-typeof': 1
+        'no-unused-expressions': 'off',
+        'babel/no-unused-expressions': 'error'
     }
 }
